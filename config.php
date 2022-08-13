af768884ece6c5ce73657bb0f406f31a9651c4fb
dan <danrleioliveira1792@gmail.com>
2022-08-12 21:46:42 -0300

    Update config.php

--- config.php
+++ config.php
@@ -2,6 +2,7 @@
     $dbHost = 'Localhost';
     $dbUsername = 'root';
     $dbPassword = 'Hesoyam134%';
+    $dbPassword = '#####';
     $dbName = 'formulario-danrlei';
 
     $conexao = new mysqli($dbHost,$dbUsername,$dbPassword,$dbName);
