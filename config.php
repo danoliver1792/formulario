ab121872a9a698ece93f91b17d27965c32efd06b
dan <danrleioliveira1792@gmail.com>
2022-08-12 21:41:04 -0300

    Create config.php

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+    $dbHost = 'Localhost';
+    $dbUsername = 'root';
+    $dbPassword = 'Hesoyam134%';
+    $dbName = 'formulario-danrlei';
+
+    $conexao = new mysqli($dbHost,$dbUsername,$dbPassword,$dbName);
+?>
